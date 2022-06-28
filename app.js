37bd45bec9fc68a8714daa189d0291857ced6552
JoaoPDeveloper <joao_entreprise@hotmail.com>
2022-06-27 23:36:56 -0300

    atividade completa

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
 const http = require('http');
-const url = require('url');
-const queryString = require('query-string');
+const queryString = require ('query-string');
+const url = require ('url');
+const fs = require ('fs'); 
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+    var resposta;
 
-    const params = queryString.parse(url.parse(req.url, true ).search);
-    let resposta;
-    if(params.pergunta =='melhor-filme'){
-        resposta = 'star wars'
+    const urlparse = url.parse(req.url, true );
+    const params = queryString.parse(urlparse.search);
+    if(urlparse.pathname == '/criar-atualizar-usuario' ){
+        
+        fs.writeFile('users/' + params.id + '.txt', JSON.stringify(params), function (err) {
+            if (err) throw err;
+            console.log('Usuário Salvo!');
+
+            resposta = 'Usuario criado/atualizado com sucesso';
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(resposta);
+        });
     }
-    else if (params.pergunta =='melhor-tecnologia-backend'){
-        resposta = 'nodejs :D'
+    else if (urlparse.pathname == '/selecionar-usuario') {
+        fs.readFile('users/' + params.id + '.txt', function(err, data){
+            resposta = data;
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(resposta);
+        });
     }
-    else{
-        resposta= 'nao sei, desculpe =(';
-    }
-     res.statusCode = 200;
-     res.setHeader('Content-Type', 'text/plain');
-     res.end(resposta);
+    
+    else if (urlparse.pathname == '/remover-usuario') {
+        fs.unlink('users/' + params.id + '.txt', function (err) {
+            console.log('File deleted!');
+
+            resposta = err ? "Usuario nao encontrado." : "Usuario removido!";
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(resposta);
+          });
+}
 });
-
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
